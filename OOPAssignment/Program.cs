de9af03d64c7afeef34cfcdace0e1f4bdf075ee0
ElenaVonder <evonder13@gmail.com>
2021-04-09 14:14:15 -0400

    fixed bugs

--- OOPAssignment/Program.cs
+++ OOPAssignment/Program.cs
@@ -15,7 +15,7 @@ namespace OOPAssignment
             {
                 new WidgetI() {Description = "small basic widget", Price = 50, Model = "Basic", Size = "Small"},
                 new WidgetI() {Description = "Med, adv widget", Price = 200, Model = "Advanced", Size = "Medium" },
-                new WidgetI() {Description = "Large, basic widget", }
+                new WidgetI() {Description = "Large, basic widget", Price = 400, Model = "Basic", }
                 new ServiceI() {Description = "Service on widget id 1", Price = 100, WidgetId = 1, ServiceYears = 1 },
                 new ServiceI() {Description = "service on widget id 2", Price = 200, WidgetId = 2, ServiceYears = 1 },
                 new ServiceI() {Description = "Service on Widget id 3", Price = 300, WidgetId = 3, ServiceYears = 2},
